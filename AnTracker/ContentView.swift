447ee8026ec5cc6f1dbd8cc6902a7321bad7e54b
zolo <root1@zolo.local>
2023-11-24 13:15:48 -0500

    new

---
+++ AnTracker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AnTracker
+//
+//  Created by zolo on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
