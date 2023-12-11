25e15af21da00ed493e42b8d73859aeef2a74042
zolo <root1@zolo.local>
2023-12-10 21:44:14 -0500

    add

--- AnTracker/AnTrackerApp.swift
+++ AnTracker/AnTrackerApp.swift
@@ -1,11 +1,7 @@
 import SwiftUI
-import Firebase
 
 @main
 struct AnTrackerApp: App {
-    init() {
-      FirebaseApp.configure()
-    }
     var body: some Scene {
         WindowGroup {
             ContentView()
