94cb2721894a05916505b0e6276326d839ac6024
zolo <root1@zolo.local>
2023-12-10 21:55:22 -0500

    hktest

---
+++ hktest/hktest/hktestApp.swift
@@ -0,0 +1,17 @@
+//
+//  hktestApp.swift
+//  hktest
+//
+//  Created by zolo on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct hktestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
